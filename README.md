c32bba4d111eb8be136cf369852c6832ad50ff0f
lualiliu <lualiliu@outlook.com>
2021-11-02 21:38:12 +0800

    modified to buildroot package

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 libgo2 - Support library for the ODROID-GO Advance
+
+
+Buildroot package
+======
+```
+################################################################################
+#
+# LIBGO2
+#
+################################################################################
+
+LIBGO2_VERSION = master
+LIBGO2_SITE = $(call github,lualiliu,libgo2,$(LIBGO2_VERSION))
+LIBGO2_INSTALL_STAGING = YES
+LIBGO2_DEPENDENCIES = host-pkgconf libpng libdrm openal librga
+
+ifeq ($(BR2_STATIC_LIBS),y)
+LIBGO2_CONF_OPTS += STATIC_ENABLED=1
+endif
+LIBGO2_MAKE_ENV = AS="$(TARGET_AS)" CC="$(TARGET_CC)" PREFIX=/usr PKG_CONFIG="$(BASE_DIR)/host/bin/pkg-config" CFLAGS="$(TARGET_CFLAGS) $(LUA_CFLAGS) -flto"
+
+define LIBGO2_BUILD_CMDS
+	$(LIBGO2_MAKE_ENV) $(MAKE) -C $(@D) $(LIBGO2_CONF_OPTS)
+endef
+
+define LIBGO2_INSTALL_STAGING_CMDS
+	mkdir -p $(STAGING_DIR)/usr/include/go2
+	$(LIBGO2_MAKE_ENV) DESTDIR="$(STAGING_DIR)" $(MAKE) -C $(@D) install-headers install-lib
+endef
+
+define LIBGO2_INSTALL_TARGET_CMDS
+	$(LIBGO2_MAKE_ENV) DESTDIR="$(TARGET_DIR)" $(MAKE) -C $(@D) install-lib
+endef
+
+$(eval $(generic-package))
+
+```
--- src/audio.c
+++ src/audio.c
@@ -1,17 +1,14 @@
 /*
 libgo2 - Support library for the ODROID-GO Advance
 Copyright (C) 2020 OtherCrashOverride
-
 This library is free software; you can redistribute it and/or
 modify it under the terms of the GNU Lesser General Public
 License as published by the Free Software Foundation; either
 version 2.1 of the License, or (at your option) any later version.
-
 This library is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 Lesser General Public License for more details.
-
 You should have received a copy of the GNU Lesser General Public
 License along with this library; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
@@ -173,7 +170,7 @@ uint32_t go2_audio_volume_get(go2_audio_t* audio)
     snd_mixer_t *handle;
     snd_mixer_selem_id_t *sid;
     const char *card = "default";
-    const char *selem_name = "Playback";
+    const char *selem_name = "Master";
 
     snd_mixer_open(&handle, 0);
     snd_mixer_attach(handle, card);
@@ -210,7 +207,7 @@ void go2_audio_volume_set(go2_audio_t* audio, uint32_t value)
     snd_mixer_t *handle;
     snd_mixer_selem_id_t *sid;
     const char *card = "default";
-    const char *selem_name = "Playback";
+    const char *selem_name = "Master";
 
     snd_mixer_open(&handle, 0);
     snd_mixer_attach(handle, card);
